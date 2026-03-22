b8b82bf651f0334c01691a0a21b0ccc881d4a6a2
Mubbale Joshua <mubsarphaxad@gmail.com>
2026-03-22 08:06:51 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCZJQ9z6jO8r2ujAIsGkztWuhWxVcqdjBs",
+    authDomain: "my-web-app-806cf.firebaseapp.com",
+    projectId: "my-web-app-806cf",
+    storageBucket: "my-web-app-806cf.firebasestorage.app",
+    messagingSenderId: "631606006607",
+    appId: "1:631606006607:web:c309b99711a78347f82b3a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
